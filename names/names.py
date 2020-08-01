23c3606e226f3138301c6035f523a7f7f25f373d
Joel <joelgroomer@gmail.com>
2020-07-31 20:12:41 -0700

    stretch goal in names.py complete

--- names/names.py
+++ names/names.py
@@ -21,7 +21,6 @@ with open('names_2.txt', 'r') as n2:
             tree = BSTNode(line)
         else:
             tree.insert(line)
-        print(f'read: {line}')
         line = n2.readline().strip()
 
 duplicates = []  # Return the list of duplicates in this data structure
@@ -51,3 +50,26 @@ Runtime of optimized code on my machine: 0.24960613250732422 seconds
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+print('======= STRETCH GOAL =======')
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = [name for name in names_1 if names_2.__contains__(name)]
+
+end_time = time.time()
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"STRETCH runtime: {end_time - start_time} seconds")
+
+"""
+Runtime of original code on my machine:  5.6630449295043945 seconds
+Runtime of optimized code on my machine: 0.24960613250732422 seconds
+Runtime of stretch goal optimized code:  1.3500370979309082 seconds
+"""
