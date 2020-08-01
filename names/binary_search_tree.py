2873bc8956cd3413cbc6b83f68a0b9917ce1db69
Joel <joelgroomer@gmail.com>
2020-07-31 19:29:51 -0700

    optimization of names.py completed by adding binary search tree

---
+++ names/binary_search_tree.py
@@ -0,0 +1,40 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # compare target to cur_value
+        # 1. ==, return true
+        # 2. <, go left
+        # 3. >, go right
+        # 4. can't go left or right, return false
+
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
@@ -6,21 +7,40 @@ f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+"""
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
+"""
+
+with open('names_2.txt', 'r') as n2:
+    line = n2.readline().strip()
+    tree = None
+    while line:
+        if tree == None:
+            tree = BSTNode(line)
+        else:
+            tree.insert(line)
+        print(f'read: {line}')
+        line = n2.readline().strip()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+"""
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+"""
+
+for name in names_1:
+    if tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
